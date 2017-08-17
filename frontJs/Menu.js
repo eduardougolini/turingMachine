aa29b84c25b60bac49b024809c4a4f29022b11f4
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-08-17 09:18:33 -0300

    Menu selecionado agora é mostrado na tela para o usuário

--- frontJs/Menu.js
+++ frontJs/Menu.js
@@ -5,6 +5,11 @@ var selectedMenu = selectorMenu;
 
 for(var i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener('click', function(){
+        for (var j = 0; j < menuItems.length; j++) {
+            menuItems[j].classList.remove('selected');
+        }
+
         selectedMenu = this.getAttribute('name');
+        this.classList.add('selected');
     });
 }
