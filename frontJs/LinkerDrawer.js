af7e2d97364de81aabe66fbf2ba364947b056656
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-10-24 16:38:13 -0200

    Adicionado suporte a linhas

--- frontJs/LinkerDrawer.js
+++ frontJs/LinkerDrawer.js
@@ -1,4 +1,5 @@
 var htmlCanvas = document.querySelector('canvas');
+var selectedObjects = [];
 
 document.body.addEventListener('click', drawLink);
 htmlCanvas.addEventListener('mousemove', identifyPosition);
@@ -9,11 +10,36 @@ function drawLink(event) {
         return;
     }
 
+    if (selectedObjects.length == 2) {
+        selectedObjects = [];
+    }
+
     for(var i in statesList) {
         if (statesList[i].hover == true) {
-            console.log('clicou dentro');
+            selectedObjects[selectedObjects.length] = statesList[i];
         }
     }
+
+    if (selectedObjects.length == 2) {
+        drawLineBetweenCircles();
+    }
+}
+
+function drawLineBetweenCircles() {
+    var canvasContext = htmlCanvas.getContext('2d');
+
+    canvasContext.strokeStyle = '#000';
+    canvasContext.lineWidth = 2;
+
+    canvasContext.beginPath();
+    canvasContext.moveTo(selectedObjects[0].x, selectedObjects[0].y);
+
+    for (var i = 0; i < 2; i++) {
+        canvasContext.lineTo(selectedObjects[i].x, selectedObjects[i].y);
+    }
+    canvasContext.arc(selectedObjects[1].x, selectedObjects[1].y, 5, 0, Math.PI * 2, true);
+
+    canvasContext.stroke();
 }
 
 function identifyPosition(event) {
@@ -30,7 +56,7 @@ function identifyPosition(event) {
 
     for(var i in statesList) {
         if(Math.pow(statesList[i].x - x, 2)+Math.pow(statesList[i].y - y, 2) < Math.pow(50, 2)) {
-            cursor = ' crosshair'
+            cursor = ' crosshair';
             statesList[i].hover = true;
         } else {
             statesList[i].hover = false;
